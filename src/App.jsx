4976308ea7cc2969bb709456a92955c5685bd1c7
Luis-Castro1 <luisangelcastrocabrera@gmail.com>
2023-09-22 14:09:49 -0500

    Principal Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='w-full h-full flex justify-center items-center bg-black'>
+      <h1 className='text-white'>Hola</h1>
+    </div>
+  )
+}
+
+export default App
